8893956c5fc25fc841d4fdd682ff8e5308e0dad2
Krista Brehm <kristabrehm1@gmail.com>
2019-01-04 18:58:18 -0500

    added a little more to the get paths

---
+++ FriendFinderFile/app/routing/apiRoutes.js
@@ -0,0 +1,6 @@
+app.get('/api/friends', displayFriends);
+
+function displayFriends(req, res) {
+    //TODO must remember how to display info from json here
+    res.send('');
+}
--- FriendFinderFile/app/routing/htmlRoutes.js
+++ FriendFinderFile/app/routing/htmlRoutes.js
@@ -1,5 +1,5 @@
 app.get('/survey', displaySurvey);
 
 function displaySurvey(req, res) {
-
+    res.send('survey.html');
 }
