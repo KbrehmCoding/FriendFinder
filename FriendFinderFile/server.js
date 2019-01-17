be7b2adfc066fb2f1d57f86e1c1fe1be8baecea8
Krista Brehm <kristabrehm1@gmail.com>
2019-01-17 14:58:47 -0500

    corrected the quote marks in the server.js as well as added to the required files in the server.js file

--- FriendFinderFile/server.js
+++ FriendFinderFile/server.js
@@ -13,9 +13,10 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+require('./app/data/friends.js')(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log('App listening on PORT: ' + PORT);
 })
